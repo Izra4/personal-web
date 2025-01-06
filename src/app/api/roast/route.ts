9b7203d0e0922938537727baab908f2a95c0fe72
Izra <aryaizra2@gmail.com>
2025-01-06 22:09:14 +0700

    feat(roast): add create roast feature

---
+++ src/app/api/roast/route.ts
@@ -0,0 +1,14 @@
+import prisma from "@/lib/prisma/prisma";
+
+export async function POST(request: Request) {
+  const { name, roast } = await request.json();
+
+  const data = await prisma.roast.create({
+    data: {
+      name,
+      review: roast
+    }
+  })
+
+  return new Response(JSON.stringify(data), { status: 201});
+};
