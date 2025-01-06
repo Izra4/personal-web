19f1e2b96c156abb36c4af885c78e293d299b9c5
Izra <aryaizra2@gmail.com>
2025-01-06 18:59:43 +0700

    feat(exp): add experience page

---
+++ src/app/experience/page.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Navbar from '../_components/navigation';
+import { Lexend, Poppins } from 'next/font/google';
+import Development from '../_components/development';
+
+const poppins = Poppins({
+  weight: [
+    '100', '200', '300', '400', 
+    '500', '600', '700', '800', '900'
+  ],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+const lexend = Lexend({
+  weight: [
+    '100', '200', '300', '400', 
+    '500', '600', '700', '800', '900'
+  ],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+const ExperiencePage: React.FC = () => {
+  return (
+    <div className="bg-primary w-screen min-h-screen lg:h-screen flex flex-col">
+      <Navbar/>
+      <Development lexend={lexend.className} poppins={poppins.className}/>
+    </div>
+  );
+};
+
+export default ExperiencePage;
