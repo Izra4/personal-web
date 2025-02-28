0af017b82369badd37d7f9cf01a5a7729bc761af
Izz <aryaizra2@gmail.com>
2025-02-28 11:16:40 +0700

    feat: add blog page

---
+++ src/app/blog/_components/blog-preview.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Image from "next/image";
+
+interface BlogPreviewProps {
+  title: string;
+  content: string;
+  imageUrl: string;
+  lexend: string;
+  poppins: string;
+}
+
+const BlogPreview = ({ title, content, imageUrl, lexend, poppins }: BlogPreviewProps) => {
+  return (
+    <div className="bg-white rounded-lg shadow-lg flex flex-col">
+      <Image
+        src={imageUrl || "/test.jpg"}
+        width={0}
+        height={0}
+        sizes="100vw"
+        alt={""}
+        style={{ width: "100%", height: "auto", borderRadius: "8px 8px 0 0" }}
+        className="p-2"
+      />
+      <div className="px-3 pb-3">
+        <div className="min-h-16 max-h-16">
+          <h2 className={`text-2xl font-semibold text-primary_text mb-2 ${lexend}`}>
+            {title || "Blog Title Here"}
+          </h2>
+        </div>
+        <div className="mt-2 font-medium min-h-24 max-h-24 mb-2">
+          <p className={`text-sm text-primary_text mb-4 ${poppins} line-clamp-5`}>
+            {content ||
+              "This is a short preview of the blog content. It gives readers a glimpse of what to expect in the full post."}
+          </p>
+        </div>
+        <div className="flex justify-end items-end">
+          <button
+            className={`${poppins} bg-[#962727] hover:bg-secondary_text text-white px-4 py-2 
+                       rounded-md hover:bg-primary-dark text-sm transition-all duration-300 ease-in-out`}
+          >
+            Details
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogPreview;
---
+++ src/app/blog/page.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import Navbar from "../_components/navigation";
+import { Lexend, Poppins } from "next/font/google";
+import BlogPreview from "./_components/blog-preview";
+
+const poppins = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const lexend = Lexend({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const BlogPage: React.FC = () => {
+  // Data contoh untuk blog
+  const blogPosts = [
+    {
+      id: 1,
+      title: "Getting Started with React ",
+      content:
+        "Learn the basics of React and how to build your first component in this introductory guide.",
+      imageUrl: "https://via.placeholder.com/400x250",
+    },
+    {
+      id: 2,
+      title: "Mastering Tailwind CSS",
+      content:
+        "Discover advanced techniques for building beautiful UIs with Tailwind CSS framework. Discover advanced techniques for building beautiful UIs with Tailwind CSS framesfdgawbgfaassaddasd",
+      imageUrl: "https://via.placeholder.com/400x250",
+    },
+    {
+      id: 3,
+      title: "Next.js for Beginners",
+      content: "An introduction to Next.js framework and how it simplifies React development. ",
+      imageUrl: "https://via.placeholder.com/400x250",
+    },
+    {
+      id: 4,
+      title: "TypeScript Tips and Tricks",
+      content:
+        "Improve your TypeScript skills with these practical tips from experienced developers.",
+      imageUrl: "https://via.placeholder.com/400x250",
+    },
+  ];
+
+  return (
+    <div className="bg-primary min-h-screen flex flex-col overflow-x-hidden">
+      <title>Iz.me | Experience</title>
+      <Navbar />
+      <div className="w-full flex-1 text-primary_text p-8 overflow-y-auto overflow-x-hidden">
+        <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+          {/* Blog Previews */}
+          {blogPosts.map((post) => (
+            <BlogPreview
+              poppins={poppins.className}
+              lexend={lexend.className}
+              key={post.id}
+              title={post.title}
+              content={post.content}
+              imageUrl={""}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogPage;
