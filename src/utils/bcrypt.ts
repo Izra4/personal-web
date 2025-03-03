575c1814c25d4d0c7c03d7ee2a2c3eab4f15bfcb
Izz <aryaizra2@gmail.com>
2025-03-03 08:22:54 +0700

    feat: add password hashing util

--- src/utils/bcrypt.ts
+++ src/utils/bcrypt.ts
@@ -1,9 +1,9 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
-export function hashPassword(password: string): Promise<string> {
-  return bcrypt.hash(password, 10);
+export async function hashPassword(password: string): Promise<string> {
+  return await bcrypt.hash(password, 10);
 }
 
-export function comparePassword(password: string, hash: string): Promise<boolean> {
-  return bcrypt.compare(password, hash);
+export async function comparePassword(password: string, hash: string): Promise<boolean> {
+  return await bcrypt.compare(password, hash);
 }
